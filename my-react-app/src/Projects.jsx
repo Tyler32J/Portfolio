64d5b4aa8d70ff8a75d608435154f7ff8460b43c
Tyler32J <tfarrell@basecampcodingacademy.org>
2026-02-12 21:17:12 -0600

    New Update

--- my-react-app/src/Projects.jsx
+++ my-react-app/src/Projects.jsx
@@ -7,35 +7,37 @@ const Projects = () => {
       title: "Sportsman's Journal",
       description: "Sportsman's Journal is a personal field log designed for hunters and anglers who value accurate records and lasting memories. Log your hunts, record your catches, and build a personal outdoor journal.",
       image: "/sportsman_journal.png",
-      link: "http://127.0.0.1:8000/"
+      // link: "http://127.0.0.1:8000/"
+      link: " https://tyler32j.github.io/Sportsman-Journal-Project/"
     },
-    {
-      id: 2,
-      title: "TJ's Handyman Service",
-      description: "Experienced handyman delivering efficient, affordable solutions for repairs, installations, and general maintenance.",
-      image: "/comingsoon.png",
-      link: ""
-    },
-    {
-      id: 3,
-      title: "Brad's Honey & Bee Removal",
-      description: "",
-      image: "/comingsoon.png",
-      link: ""
-    },
+    // {
+    //   id: 2,
+    //   title: "TJ's Handyman Service",
+    //   description:"Still in Development",
+    //   // description: "Experienced handyman delivering efficient, affordable solutions for repairs, installations, and general maintenance.",
+    //   image: "/comingsoon.png",
+    //   link: ""
+    // },
+    // {
+    //   id: 3,
+    //   title: "Brad's Honey & Bee Removal",
+    //   description: "Still in Development",
+    //   image: "/comingsoon.png",
+    //   link: ""
+    // },
     {
       id: 4,
       title: "Betting on the Races!",
       description: "Put your racing knowledge to the test by betting on your favorite drivers. Pick who you think will win and place your bet,",
       image: "/betting_on_the_races.png",
-      link: "http://localhost:3000/Betting-On-The-Races-React"
+      link: "https://github.com/jrsossaman/Betting-On-The-Races-React"
     },
     {
       id: 5,
       title: "The Goat",
       description: "Master Chief is more than a warrior; he is a symbol of hope and courage. In the face of impossible odds, he stands unbroken, fighting not for himself, but for the survival of humanity.",
       image: "/halo_background.png",
-      link: "http://127.0.0.1:5500/"
+      link: "https://github.com/Tyler32J/GOAT"
     },
     // {
     //   id: 6,
